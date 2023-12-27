99bd9dfd97a5352a130b3fb9e8a10a2237be5e8a
ChelseyBommer <chelsey.bommer@hva.nl>
2023-12-27 10:56:20 +0100

    add: root colors

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template> 
+    <h1> {{title}} </h1>
+    <input type="text" ref="name">
+    <button @click="handleClick">click me</button>
+    <Modal/>
+</template>
+
+<script>
+ import Modal from './components/modal.vue'
+
+ export default{
+    name: 'App',
+    components:{ Modal },
+    data(){
+        return{
+            title: 'My first vue app'
+        }
+    },
+    methods:{
+        handleClick(){
+            console.log(this.$refs.name)
+            this.$refs.name.classList.add('active')
+        }
+    }
+}
+</script>
