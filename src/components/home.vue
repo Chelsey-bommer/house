19c7ef8994053417c8fb154c6342d79472e7c637
ChelseyBommer <chelsey.bommer@hva.nl>
2023-12-27 15:01:26 +0100

    update: access to houses API

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,29 +4,15 @@
 
   
   <div>
-    <div v-for="post in posts" v-bind:key="post.id">
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.body }}</p>
-    </div>
+    
+    {{ houseId }}
   </div>
 
     
 </template>
 
 <script>
-    var myHeaders = new Headers();
-    myHeaders.append("X-Api-Key", "{api_key}");
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    fetch("https://api.intern.d-tt.nl/api/houses", requestOptions)
-     .then(response => response.text())
-     .then(result => console.log(result))
-     .catch(error => console.log('error', error));
+   
 
     export default{
         props: ['header', 'text'],
@@ -35,9 +21,7 @@
         
     },
 
-    created() {
-        this.getData();
-    },
+   
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+
 import { createApp } from "vue"
 import App from './app.vue'
 import './assets/global.css'
 import { createRouter, createWebHistory } from 'vue-router'
+
+
 
 import Home from './components/home.vue'
 import About from './components/about.vue'
@@ -14,6 +17,20 @@ const router = createRouter({
         { path: '/about', component: About },
     ]
 });
+
+var myHeaders = new Headers();
+myHeaders.append("X-Api-Key", "3s8K-HjLw1vBgI2nelJFMWbcQ6upXDYm");
+
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+};
+
+fetch("https://api.intern.d-tt.nl/api/houses", requestOptions)
+ .then(response => response.text())
+ .then(result => console.log(result))
+ .catch(error => console.log('error', error));
 
 createApp(App)
   .use(router)
