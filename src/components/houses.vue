487b39a6fee36304182d528662bb872cfcb2abab
ChelseyBommer <chelsey.bommer@hva.nl>
2024-01-02 11:40:37 +0100

    add: sort function price 1 button

--- src/components/houses.vue
+++ src/components/houses.vue
@@ -1,29 +1,24 @@
 <template>
-    
-
     <div>
         <h1>Houses</h1>
-        <div v-for="houses in houseList" class="houseslist">
+        <button @click="sortHousesByPrice">Sort by Price</button>
+        <div v-for="houses in sortedHouseList" class="houseslist">
             <img :src="houses.image" alt="" :key="houses.id">
             <div>
-             <h2>{{ houses.location.city }} {{ houses.location.street }} {{ houses.location.houseNumber }}</h2>
-             <p> {{ houses.price }}</p>
-             <p> {{ houses.location.zip }}</p>
-             <router-link :to="{ name:'houseDetails', params:{id: houses.id}}" > See tha house</router-link>
+                <h2>{{ houses.location.city }} {{ houses.location.street }} {{ houses.location.houseNumber }}</h2>
+                <p> {{ houses.price }}</p>
+                <p> {{ houses.location.zip }}</p>
+                <router-link :to="{ name: 'houseDetails', params: { id: houses.id } }"> See tha house</router-link>
             </div>
-            
-            
-         </div>
 
-        
+        </div>
 
     </div>
 </template>
 
 
 <script>
-
-
+import { computed, ref, onMounted  } from 'vue';
 
 
 var myHeaders = new Headers();
@@ -43,51 +38,64 @@ var requestOptions = {
 
 export default {
     props: ['header', 'text'],
-    data(){
-        return{
+    data() {
+        return {
             houseList: [],
-            searchHouses: ''
+            searchHouses: '',
+            sortBy: 'price', // Default sorting by price
+            sortOrder: 'asc', // Default sorting order (ascending)
         }
     },
-    computed:{
-  
+
+    computed: {
+        sortedHouseList() {
+            return this.houseList.slice().sort((a, b) => {
+                const order = this.sortOrder === 'asc' ? 1 : -1;
+                return order * (a[this.sortBy] - b[this.sortBy]);
+            });
+        },
     },
 
     methods: {
-        async getData(){
+        async getData() {
             const res = await fetch("https://api.intern.d-tt.nl/api/houses", requestOptions)
             const finalRes = await res.json();
             this.houseList = finalRes
-        }
+        },
+
+        async sortHousesByPrice() {
+            // Toggle sorting order when the button is clicked
+            this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+        },
     },
     mounted() {
-      this.getData()
+        this.getData()
     }
 
 }
 </script>
 
 <style scoped>
-.houseslist{
+.houseslist {
     background-color: var(--background-color2);
 
-    width:60%;
-    margin-left:auto;
-    margin-right:auto;
-    margin-bottom:1em;
-    padding:1em;
+    width: 60%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 1em;
+    padding: 1em;
 
     border-radius: 5px;
 
-    display:flex;
+    display: flex;
 }
 
-.houseslist img{
-    width:200px;
+.houseslist img {
+    width: 200px;
 }
 
-h1{
-    margin-left:19%;
-    margin-top:1em;
+h1 {
+    margin-left: 19%;
+    margin-top: 1em;
 }
 </style>
